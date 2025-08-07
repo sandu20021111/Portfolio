b0797bafc38e69d0646959a2ba79f695a350dced
Sanduni Vihara Walage <sandunivihara228@gmail.com>
2025-08-07 22:43:44 +0530

    update

--- myportfolio/src/components/ContactForm.jsx
+++ myportfolio/src/components/ContactForm.jsx
@@ -152,7 +152,7 @@ const Contact = () => {
                 <Github className="w-6 h-6" />
               </a>
               <a
-                href="https://linkedin.com/in/yourusername"
+                href="https://www.linkedin.com/in/sanduni-vihara-56990528b"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:text-green-400 transition"
@@ -160,7 +160,7 @@ const Contact = () => {
                 <Linkedin className="w-6 h-6" />
               </a>
               <a
-                href="https://instagram.com/sandu.vihara2002"
+                href="https://instagram.com/sandu_vihara2002"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:text-green-400 transition"
