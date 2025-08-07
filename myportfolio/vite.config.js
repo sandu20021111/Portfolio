591b36df23528ac5885ddad2a8e698ddc7089fe9
Sanduni Vihara Walage <sandunivihara228@gmail.com>
2025-08-08 00:04:06 +0530

    update

--- myportfolio/vite.config.js
+++ myportfolio/vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const repoName = "github.com/sandu20021111/portfolio";
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: `/${repoName}/`,
+  base: "/Portfolio/",
 });
